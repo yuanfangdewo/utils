a9112cb5f5a37d6221d9bf0c12364fabe990c76a
yuanfangdewo <zhuhaoyuan2012@139.com>
2019-03-27 20:22:24 +0800

    Update func.go

--- array/func.go
+++ array/func.go
@@ -62,3 +62,21 @@ func ArrayEqual(a, b []string) bool {
 
 	return true
 }
+
+func InArray(a []string, x string) bool {
+    for _, n := range a {
+        if x == n {
+            return true
+        }
+    }
+    return false
+}
+
+func Contains(a []string, x string) bool {
+    for _, n := range a {
+        if x == n {
+            return true
+        }
+    }
+    return false
+}
